45efd06ac22499bb856757f4406a4047066ca193
Burak İdi <burak.idi@yandex.com>
2023-05-06 21:45:17 +0300

    v3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,6 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
         //V1 çalışmalarım
         //v2 üzerine çalışmalar...
+        System.out.println("v3 kodları...");//v3
 
 
 
